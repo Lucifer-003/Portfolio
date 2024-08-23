e873d818325f42b50441239ba712cd0f51ca3aad
Lucifer-003 <harshdubey30102002@gmail.com>
2024-08-23 13:55:10 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings:["Frontend Developer","Machine Learning Developer","Full Stack Developer"],
+    typeSpeed:50,
+    backSpeed:70,
+    backDelay:1000,
+    loop:true
+});
